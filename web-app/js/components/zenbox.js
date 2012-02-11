2ce7b78afa993051962356e4709d52e2b73127d1
gil.fernandes@gmail.com <gil.fernandes@gmail.com@6a28ef4a-0c77-d166-fb91-bc409297a1aa>
2012-02-11 14:21:59 +0000

    Positioning improvements on resize of the zenbox.

--- web-app/js/components/zenbox.js
+++ web-app/js/components/zenbox.js
@@ -1,36 +1,53 @@
 
-
+/**
+ * Contains methods for rendering a box on the side used to switch either to 
+ * the designer or to the workbench.
+ */
 var zenbox = {
     
     /**
      * Creates a fixed floater on the side.
+     * @param isWorkbench If {@code true} the target link will be for the designer pager,
+     * else for the workbench page.
+     * @param userRole If ADMIN or USER the fixed floater is created, else not.
      */
-    createBox: function(isWorbench) {
+    createBox: function(isWorbench, userRole) {
         var viewPortSize = screenSize.calculateViewPort();
         var target = (isWorbench ? 'designer' : 'workbench');
         var imgOffset = -5;
+        var imgHeight = 137;
         var zenboxLabel = isc.Label.create({
             ID:"zenboxLabel",
             shadowOffset: 0,
             padding:5,
             valign:"center",
             align:"center",
-            canDragReposition: false, 
+            canDragReposition: false,
             dragAppearance:"target",
             width: 49,
-            height: 137,
-            top: (viewPortSize.height - 137) / 2, 
+            height: imgHeight,
+            top: (viewPortSize.height - imgHeight) / 2, 
             left: imgOffset, // start off-screen
             zIndex: 1000,
             contents: '<div>\
                       <a href="' + target + '.gsp">\
-                      <img src="images/zenbox_' + target + '.png"></a>\
+                      <img border="0" src="images/zenbox_' + target + '.png"></a>\
                  </div>',
             mouseOver: function() { this.animateMove(0, zenboxLabel.top) },
             mouseOut: function() { this.animateMove(imgOffset, zenboxLabel.top) },
-            prompt: "Switch to <b>" + target + "</b>",
-            hoverOpacity: 90,
-            hoverWidth: 120
+            redrawOnResize: true,
+            redraw: function() {
+                var viewPortSize = screenSize.calculateViewPort();
+                this.top = (viewPortSize.height - imgHeight) / 2;
+                this.Super("redraw",arguments);
+                return this;
+            },
+            init: function() {
+                this.Super("init", arguments);
+                isc.Page.setEvent("resize", function() {
+                    zenboxLabel.markForRedraw();
+                });
+            }
         });
         zenboxLabel.bringToFront();
     }
